33b5cfbeb5156d4fdb9ddc461834e4d86de6cdbe
joevalook <joevalookaran@hotmail.com>
2023-03-09 15:49:43 -0500

    added helper functions in weather service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,63 @@ import { WeatherData } from './models/weather.model';
 import { WeatherService } from './services/weather.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  constructor(private weatherService: WeatherService) {
+export class AppComponent implements OnInit {
+	constructor(private weatherService: WeatherService) {
 
-  }
-  title = 'WeatherApp';
-  n = 13;
-  system = 'Metric'
-  weatherData?: WeatherData
-  containerStyle = "containerHotDay" //change to object
-  ngOnInit() {
-		this.weatherService.getWeatherData('honolulu')
-		.subscribe({
-			next: (response) => {
-				this.weatherData = response;
-				console.log(response)
-				console.log(response.current.condition.text.toLowerCase().includes('thunder') ? 'thunder': '');
-				if (response.current.condition.text.toLowerCase().includes('thunder')) {
-					this.containerStyle="containerThunder";
-					document.body.style.backgroundColor = "rgb(176, 123, 77);";//change back to thunder
-					console.log((<any>document.getElementsByClassName("container"))) 
+	}
+	title = 'WeatherApp';
+	n = 13;
+	system = 'Metric'
+	weatherData?: WeatherData
+	containerStyle = "containerColdNight"; //change to object
+	
+	ngOnInit() {
+		this.weatherService.getWeatherData('El Centro')
+			.subscribe({
+				next: (response) => {
+					this.weatherData = response;
+					console.log(response.current.humidity);
+					console.log(this.weatherService.isDay(response))
+					
+					if (response.current.condition.text.toLowerCase().includes('thunder')) {
+						this.containerStyle = "containerThunder";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('snow') && this.weatherService.isDay(response)) {
+						this.containerStyle = "containerSnowDay";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('snow') && !this.weatherService.isDay(response)) {
+						this.containerStyle = "containerSnowNight";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('rain') && this.weatherService.isDay(response)) {
+						console.log('hi')
+						this.containerStyle = "containerRainDay";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('rain') && !this.weatherService.isDay(response)) {
+						this.containerStyle = "containerRainNight";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('fog')) {
+						this.containerStyle = "containerFoggy";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('cloud') && this.weatherService.isDay(response)) {
+						this.containerStyle = "containerCloudyDay";
+					}
+					else if (response.current.condition.text.toLowerCase().includes('cloud') && !this.weatherService.isDay(response)) {
+						this.containerStyle = "containerCloudyNight";
+					}
+					else if (Number(response.current.humidity) < 40 && Number(response.current.temp_c) > 30 && this.weatherService.isDay(response)) {
+						this.containerStyle = "containerDesertDay";
+					}
+
+
+					else if (response.current.condition.text.toLowerCase().includes('cloud') && !this.weatherService.isDay(response)) {
+						this.containerStyle = "containerCloudyNight";
+					}
+					document.body.style.backgroundColor = this.weatherService.bodyColor(this.containerStyle);
 				}
-				if (response.current.condition.text.toLowerCase().includes('thunder')) {
-					this.containerStyle="containerThunder";
-					document.body.style.backgroundColor = "rgb(176, 123, 77);";//change back to thunder
-					console.log((<any>document.getElementsByClassName("container"))) 
-				}
-			}
-		})
+			})
 	}
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -21,4 +21,80 @@ export class WeatherService {
       .set('days', 1)
     })
   }
+  isDay(response: WeatherData): Boolean {
+		let currentTime = response.location.localtime.split(" ")[1]
+		if (currentTime[1] ===':') {
+			currentTime = '0' + response.location.localtime.split(" ")[1]
+		}
+		console.log("Current time: " + currentTime)
+		let sunrise = response.forecast.forecastday[0].astro.sunrise
+		if (sunrise[sunrise.length - 2] === 'A'){
+			sunrise = sunrise.substring(0, 5)
+		}
+		else {
+			sunrise = String(Number(sunrise.substring(0,2)) + 12) + sunrise.substring(2,5);
+		}
+		console.log("Sunrise " + sunrise)
+		let sunset = response.forecast.forecastday[0].astro.sunset
+		if (sunset[sunset.length - 2] === 'A'){
+			sunset = sunset.substring(0, 5)
+		}
+		else {
+			sunset = String(Number(sunset.substring(0,2)) + 12) + sunset.substring(2,5);
+		}
+		console.log("Sunset " + sunset)
+		if (currentTime > sunrise && currentTime < sunset) {
+			return true
+		}
+		else {
+			return false
+		}
+	}
+  bodyColor(color: string): string {
+    if (color === 'containerThunder'){
+      return('rgb(79, 77, 100)')
+    }
+    else if (color === 'containerSnowDay'){
+      return('rgb(83, 170, 196)')
+    }
+    else if (color === 'containerSnowNight'){
+      return('rgb(26, 95, 118)')
+    }
+    else if (color === 'containerRainDay'){
+      return('rgb(53, 123, 147)')
+    }
+    else if (color === 'containerRainNight'){
+      return('rgb(63, 133, 156)')
+    }
+    else if (color === 'containerFoggy'){
+      return('rgb(56, 196, 209)')
+    }
+    else if (color === 'containerCloudyDay'){
+      return('rgb(103, 210, 227)')
+    }
+    else if (color === 'containerCloudyNight'){
+      return('rgb(36, 51, 95)')
+    }
+    else if (color === 'containerDesertDay'){
+      return('rgb(219, 124, 41)')
+    }
+    else if (color === 'containerWindyDay'){
+      return('rgb(70, 102, 209)')
+    }
+    else if (color === 'containerHotDay'){
+      return('rgb(176, 123, 77)')
+    }
+    else if (color === 'containerColdDay'){
+      return('rgb(22, 152, 222)')
+    }
+    else if (color === 'containerColdNight'){
+      return('rgb(67, 141, 220)')
+    }
+    else if (color === 'containerRegularDay'){
+      return('rgb(67, 141, 220)')
+    }
+    else {
+      return('rgb(25, 109, 77)')
+    }
+  }
 }
