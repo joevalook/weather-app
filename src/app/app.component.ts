64164da112c3a858c5e4fb5c9a3eafde840a6783
joevalook <joevalookaran@hotmail.com>
2023-03-08 20:09:25 -0500

    finished hooking up the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private weatherService: WeatherService) {
+
+  }
   title = 'WeatherApp';
   n = 13;
+  system = 'Metric'
+  weatherData?: WeatherData
+  ngOnInit() {
+		this.weatherService.getWeatherData('Yakutsk')
+		.subscribe({
+			next: (response) => {
+				this.weatherData = response;
+				console.log(response);
+			}
+		})
+	}
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'a040fca44cmsh8238c4f5cbf4b08p1a0efdjsnc017f1f79da2'
+};
